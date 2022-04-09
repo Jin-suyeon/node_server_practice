6c1f09c6d742ec4e09093667a89d2af20b0eb363
Jin-suyeon <wlstndus99@gmail.com>
2022-04-09 17:29:21 +0900

    Feature: login 구현중

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 app.set("view engine", "ejs");
+
+app.use("/public", express.static("public"));
 
 let db;
 MongoClient.connect(
@@ -20,11 +25,11 @@ MongoClient.connect(
 );
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index.ejs");
 });
 
 app.get("/write", (req, res) => {
-  res.sendFile(__dirname + "/write.html");
+  res.render("write.ejs");
 });
 
 app.get("/list", (req, res) => {
@@ -64,3 +69,83 @@ app.post("/list", (req, res) => {
     });
   });
 });
+
+app.put("/list", (req, res) => {
+  console.log(req.body);
+  db.collection("post").updateOne(
+    { _id: parseInt(req.body.id) },
+    { $set: { title: req.body.title, date: req.body.date } },
+    (err, resp) => {
+      res.redirect("/list");
+    }
+  );
+});
+
+app.get("/detail/:id", (req, res) => {
+  db.collection("post").findOne(
+    { _id: parseInt(req.params.id) },
+    (err, resp) => {
+      res.render("detail.ejs", { data: resp });
+    }
+  );
+});
+
+app.get("/edit/:id", (req, res) => {
+  db.collection("post").findOne(
+    { _id: parseInt(req.params.id) },
+    (err, resp) => {
+      res.render("edit.ejs", { data: resp });
+    }
+  );
+});
+
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const session = require("express-session");
+const { suppressDeprecationWarnings } = require("moment");
+
+app.use(
+  session({ secret: "비밀코드", resave: true, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/fail" }),
+  (req, res) => {
+    res.redirect("/");
+  }
+);
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      session: true,
+      passReqToCallback: false,
+    },
+    function (입력한아이디, 입력한비번, done) {
+      //console.log(입력한아이디, 입력한비번);
+      db.collection("login").findOne(
+        { id: 입력한아이디 },
+        function (에러, 결과) {
+          if (에러) return done(에러);
+
+          if (!결과)
+            return done(null, false, { message: "존재하지않는 아이디요" });
+          if (입력한비번 == 결과.pw) {
+            return done(null, 결과);
+          } else {
+            return done(null, false, { message: "비번틀렸어요" });
+          }
+        }
+      );
+    }
+  )
+);
